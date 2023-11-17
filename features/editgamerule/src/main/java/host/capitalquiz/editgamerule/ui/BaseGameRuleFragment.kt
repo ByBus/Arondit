fb943f24e2198b5f987479bd6eaaf57bdbcf886a
Alex <al3xxxgm@gmail.com>
2023-11-18 00:07:37 +0300

    show fab on dragging recyclerview

--- features/editgamerule/src/main/java/host/capitalquiz/editgamerule/ui/BaseGameRuleFragment.kt
+++ features/editgamerule/src/main/java/host/capitalquiz/editgamerule/ui/BaseGameRuleFragment.kt
@@ -7,17 +7,33 @@ import androidx.viewbinding.ViewBinding
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import host.capitalquiz.core.ui.BindingFragment
 
-abstract class BaseGameRuleFragment<VB: ViewBinding>: BindingFragment<VB>() {
+abstract class BaseGameRuleFragment<VB : ViewBinding> : BindingFragment<VB>() {
     abstract val fab: ExtendedFloatingActionButton
     abstract val recyclerView: RecyclerView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerView.addOnScrollListener(
+            object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    if (newState == RecyclerView.SCROLL_STATE_DRAGGING && fab.isShown.not()) {
+                        fab.show()
+                    }
+                }
 
-        recyclerView.setOnScrollChangeListener { _, _, scrollY, _, oldScrollY ->
-            val dy = scrollY - oldScrollY
-            if (dy > 10 && fab.isShown) fab.hide()
-            if (dy < -10 && fab.isShown.not()) fab.show()
-        }
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (dy > 10 && fab.isShown) fab.hide()
+                    if (dy < -10 && fab.isShown.not()) fab.show()
+                }
+            }
+        )
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        recyclerView.clearOnScrollListeners()
+    }
+
 }
