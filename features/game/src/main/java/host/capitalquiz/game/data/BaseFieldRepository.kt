aa57c700fae08a217ade1719e1e765d6098ed9de
Alex <al3xxxgm@gmail.com>
2024-04-19 15:58:00 +0300

    add coroutine dispatcher dependency for field repo

--- features/game/src/main/java/host/capitalquiz/game/data/BaseFieldRepository.kt
+++ features/game/src/main/java/host/capitalquiz/game/data/BaseFieldRepository.kt
@@ -3,11 +3,12 @@ package host.capitalquiz.game.data
 import host.capitalquiz.core.db.FieldDao
 import host.capitalquiz.core.db.FieldData
 import host.capitalquiz.core.db.mappers.FieldDataMapperWithParameter
+import host.capitalquiz.game.di.IoDispatcher
 import host.capitalquiz.game.domain.Field
 import host.capitalquiz.game.domain.FieldRepository
 import host.capitalquiz.game.domain.GameRuleRepository
 import host.capitalquiz.game.domain.GameRuleSimple
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
@@ -18,6 +19,8 @@ class BaseFieldRepository @Inject constructor(
     private val fieldDao: FieldDao,
     private val mapper: FieldDataMapperWithParameter<GameRuleSimple, Field>,
     private val gameRuleRepository: GameRuleRepository,
+    @IoDispatcher
+    private val dispatcher: CoroutineDispatcher,
 ) : FieldRepository {
 
     override fun allFieldsOfGame(gameId: Long): Flow<List<Field>> {
@@ -36,7 +39,7 @@ class BaseFieldRepository @Inject constructor(
     }
 
     override suspend fun fieldsOfGame(gameId: Long): List<Field> {
-        return withContext(Dispatchers.IO) {
+        return withContext(dispatcher) {
             allFieldsOfGame(gameId).first()
         }
     }
